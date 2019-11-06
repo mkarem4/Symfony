cdca04d8c5882bcfa3bbef604818504d2d35524f
Mohamed Karem <m.karem@altairegypt.com>
2019-11-06 09:27:14 +0200

    add new comment show all posts and post details

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -2,7 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Exception as ExceptionAlias;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SiteController extends AbstractController
@@ -10,10 +17,51 @@ class SiteController extends AbstractController
     /**
      * @Route("/site", name="site")
      */
-    public function index()
+    public function index(PostRepository $postRepository)
     {
+        $posts = $postRepository->findAll();
         return $this->render('site/index.html.twig', [
-            'controller_name' => 'SiteController',
+            'posts' => $posts,
         ]);
     }
+
+    /**
+     * @Route("/post/{id}", name="post_details", methods={"GET"})
+     */
+    public function show(Post $post): Response
+    {
+        $comments = $post->getComments();
+        return $this->render('site/show.html.twig', [
+            'post' => $post,
+            'comments' => $comments,
+        ]);
+    }
+
+    /**
+     * @Route("/comment/{id}", name="save_comment", methods={"POST"})
+     * @param Request $request
+     * @param $id
+     * @return RedirectResponse|Response
+     * @throws ExceptionAlias
+     */
+    public function save(Request $request, $id)
+    {
+        $post = $this->getDoctrine()->getRepository(Post::class)->find($id);
+        $comments = $post->getComments();
+        $comment = new Comment();
+        if ($request) {
+            $comment->setContent($request->get('content'));
+            $comment->setCreatedAt(new \DateTime());
+            $comment->setUpdatedAt(new \DateTime());
+            $comment->setUser($this->getUser());
+            $comment->setPost($post);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($comment);
+            $em->flush();
+            return $this->redirect($this->generateUrl('post_details', array('id' => $id)), 301);
+        }
+
+        return $this->render('site/show.html.twig', array('post' => $post, 'comments' => $comments));
+    }
+
 }
