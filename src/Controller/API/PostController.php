9159cb22056a2abc7c9031dae8544fe75358a1c6
Maysa Ahmed <m.ahmed@altairegypt.com>
2019-11-06 14:15:48 +0200

    api

--- src/Controller/API/PostController.php
+++ src/Controller/API/PostController.php
@@ -7,13 +7,43 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use App\Entity\Post;
 use App\Form\PostType;
+use App\Repository\PostRepository;
 /**
  * Post controller.
  * @Route("/api", name="api_")
  */
 class PostController extends FOSRestController
 {
+
     /**
+     * Retrieves an post resource
+     * @Rest\Get("/posts/{postId}")
+     * @param Int $postId
+     * @return View
+     */
+    public function getPost(int $postId): View
+    {
+        $article = $this->PostRepository->findById($postId);
+        // In case our GET was a success we need to return a 200 HTTP OK response with the request object
+        return View::create($article, Response::HTTP_OK);
+    }
+        /**
+     * Creates an Article resource
+     * @Rest\Post("/posts")
+     * @param Request $request
+     * @return View
+     */
+    public function postArticle(Request $request): View
+    {
+        $post = new Post();
+        $post->setTitle($request->get('title'));
+        $post->setContent($request->get('content'));
+        $this->postRepository->save($post);
+        // In case our POST was a success we need to return a 201 HTTP CREATED response
+        return View::create($post, Response::HTTP_CREATED);
+    }
+
+        /**
      * Lists all posts.
      * @Rest\Get("/posts")
      *
