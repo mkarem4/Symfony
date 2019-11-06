adf8c7c3b181bb1718b0ebed660dfe0123a323cc
Maysa Ahmed <m.ahmed@altairegypt.com>
2019-11-06 12:17:49 +0200

    start api config

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Impulze\Bundle\InterventionImageBundle\ImpulzeInterventionImageBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
